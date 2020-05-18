48e4a7779daf1b773b8eeabbe6e24e148914b2e5
Todor Gyumyushev <yodor1@gmail.com>
2020-05-18 03:15:28 +0300

    refactoring
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- class/pages/AdminPage.php
+++ class/pages/AdminPage.php
@@ -1,12 +1,12 @@
 <?php
-include_once("pages/AdminPageLib.php");
+include_once("pages/SparkAdminPage.php");
 include_once("components/MenuBarComponent.php");
 
 include_once("components/renderers/cells/BeanFieldCellRenderer.php");
 include_once("components/renderers/cells/CallbackTableCellRenderer.php");
 include_once("components/renderers/cells/BooleanFieldCellRenderer.php");
 
-class AdminPage extends AdminPageLib
+class AdminPage extends SparkAdminPage
 {
 
     public function __construct()
@@ -15,7 +15,7 @@ class AdminPage extends AdminPageLib
         MenuItem::$icon_path = SPARK_LOCAL . "/images/admin/spark_icons/";
 
         $this->addMeta("viewport", "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0");
-        $this->addCSS(LOCAL . "/css/admin.css");
+        $this->addCSS(LOCAL . "/css/AdminPage.css");
     }
 
 }

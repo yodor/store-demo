03c8dd160b47e7bb492b4520ff79e98d1d9cdb1a
Todor Gyumyushev <yodor1@gmail.com>
2020-05-19 03:34:57 +0300

    refactoring
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- admin/content/menus/main/list.php
+++ admin/content/menus/main/list.php
@@ -5,10 +5,12 @@ include_once("templates/admin/NestedSetViewPage.php");
 include_once("beans/MenuItemsBean.php");
 
 $cmp = new NestedSetViewPage();
-$cmp->setBean(new MenuItemsBean());
 
 //will use "menu_title" to set the label of the TreeView Item
 $cmp->setListFields(array("menu_title" => "MenuTitle"));
+
+$cmp->setBean(new MenuItemsBean());
+
 
 $cmp->render();
 

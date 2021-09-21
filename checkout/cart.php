8081e06bdffb79f859565610741d6e4520585d6c
Todor Gyumyushev <yodor1@gmail.com>
2021-09-22 01:35:53 +0300

    updates cart listeners
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- checkout/cart.php
+++ checkout/cart.php
@@ -3,110 +3,82 @@ include_once("session.php");
 include_once("class/pages/CheckoutPage.php");
 include_once("store/utils/cart/Cart.php");
 include_once("store/beans/SellableProducts.php");
+include_once("store/utils/cart/ICartListener.php");
 
 $page = new CheckoutPage();
 $page->modify_enabled = TRUE;
 
-
 $cart = Cart::Instance();
-
-if (isset($_GET["clear"])) {
-    $cart->clear();
-    $cart->store();
-    header("Location: cart.php");
-    exit;
-}
 
 $num = -1;
 $piID = -1;
 
-if (isset($_GET["piID"])) {
-    $piID = (int)$_GET["piID"];
-}
+try {
 
+    $redirect = FALSE;
 
-//client adds product item to cart (lands from product details page)
-if (isset($_GET["add"])) {
+    if (isset($_GET["piID"])) {
+        $piID = (int)$_GET["piID"];
 
-    $bean = new SellableProducts();
-    $query = $bean->query("piID");
-    $query->select->where()->add("piID", $piID);
-    $query->select->limit = " 1 ";
+        $bean = new SellableProducts();
+        $query = $bean->query("piID");
+        $query->select->where()->add("piID", $piID);
+        $query->select->limit = " 1 ";
 
-    $num = $query->exec();
+        $num = $query->exec();
 
-    if ($num<1) {
-        $cart->remove($piID);
-        $cart->store();
+        if ($num < 1) {
+            throw new Exception("SellableProducts returned non positive result count");
+        }
 
-        Session::SetAlert(tr("Продуктът не е достъпен за продажба"));
-        header("Location:cart.php");
-        exit;
-    }
+        //client add product to cart
+        else if (isset($_GET["add"])) {
 
-    //increment amount
-    if ($cart->contains($piID)) {
-        $item = $cart->get($piID);
-        $item->increment();
-    }
-    else {
-        try {
             if ($result = $query->nextResult()) {
+
+                $stock_amount = $result->get("stock_amount");
+                if ($stock_amount < 1) throw new Exception("SellableProducts returned stock amount low");
+
                 $item = new CartItem($result->get("piID"), $result->get("sell_price"));
                 $cart->addItem($item);
+                $redirect = TRUE;
+            }
+            else {
+                throw new Exception("Unable to fetch required SellableProduct to do add");
             }
         }
-        catch (Exception $e) {
-            debug("Error adding product to cart: ".$e->getMessage());
+        //client increase product item amount from the + button
+        else if (isset($_GET["increment"])) {
+            $cart->increment($piID);
+            $redirect = TRUE;
         }
-    }
-    $cart->store();
-    header("Location: cart.php");
-    exit;
-
-//manage stock amount is disabled
-//        if ($item["stock_amount"] < 1) {
-//            Session::SetAlert(tr("Съжаляваме в момента няма наличност от този артикул"));
-//            header("Location: " . LOCAL . "/details.php?prodID=$prodID&piID=$piID");
-//            exit;
-//        }
-//        if ($item["stock_amount"] - $page->getCart()->getItemQty($piID) - 1 < 0) {
-//            Session::SetAlert(tr("Няма повече наличност от този артикул"));
-//        }
-//        else {
-//        }
-}
-//client increase product item amount from the + button
-else if (isset($_GET["increment"])) {
-    if ($cart->contains($piID)) {
-        $item = $cart->get($piID);
-        $item->increment();
-        $cart->store();
-        header("Location: cart.php");
-        exit;
-    }
-}
-//client decrease product item amount from the - button
-else if (isset($_GET["decrement"])) {
-    if ($cart->contains($piID)) {
-        $item = $cart->get($piID);
-        $item->decrement();
-        if ($item->getQuantity()<1) {
+        //client decrease product item amount from the - button
+        else if (isset($_GET["decrement"])) {
+            $cart->decrement($piID);
+            $redirect = TRUE;
+        }
+        //client remove product item from the x button
+        else if (isset($_GET["remove"])) {
             $cart->remove($piID);
+            $redirect = TRUE;
         }
+    }
+    else if (isset($_GET["clear"])) {
+        $cart->clear();
+        $redirect = true;
+    }
+
+    if ($redirect) {
         $cart->store();
         header("Location: cart.php");
         exit;
     }
 }
-else if (isset($_GET["remove"])) {
-    $cart->remove($piID);
-    $cart->store();
+catch (Exception $e) {
+    Session::SetAlert("Грешка: " . $e->getMessage());
     header("Location: cart.php");
     exit;
 }
-
-
 
 $page->startRender();
 

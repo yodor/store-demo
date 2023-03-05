2a3f969a8501ac630e5f04506bdc5b94c4461da3
Todor Gyumyushev <yodor1@gmail.com>
2023-03-05 06:08:08 +0200

    updates: changeable logo
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

---
+++ logo.php
@@ -0,0 +1,22 @@
+<?php
+include_once("session.php");
+include_once("utils/SparkFile.php");
+include_once("storage/SparkFileHTTPResponse.php");
+
+$user_logo = new SparkFile("logo");
+$user_logo->setPath(CACHE_PATH);
+
+$response = new SparkFileHTTPResponse();
+if ($user_logo->exists()) {
+    $response->setFile($user_logo);
+    $response->send();
+    exit;
+}
+else {
+    $origin_logo = new SparkFile("logo_header.origin");
+    $origin_logo->setPath(INSTALL_PATH);
+    $response->setFile($origin_logo);
+    $response->send();
+    exit;
+}
+?>

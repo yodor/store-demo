464ca73271b27feddb2c3b2e89e28bd883323e62
Todor Gyumyushev <yodor1@gmail.com>
2024-08-30 04:07:16 +0300

    updates/fixes DataBuffer

--- logo.php
+++ logo.php
@@ -4,20 +4,18 @@ define("SKIP_DB",1);
 define("SKIP_TRANSLATOR",1);
 include_once("session.php");
 include_once("utils/SparkFile.php");
-include_once("storage/SparkFileHTTPResponse.php");
+include_once("storage/SparkHTTPResponse.php");
 
 $user_logo = new SparkFile("logo");
 $user_logo->setPath(CACHE_PATH);
 
-$response = new SparkFileHTTPResponse();
+$response = new SparkHTTPResponse();
 if ($user_logo->exists()) {
-    $response->setFile($user_logo);
-    $response->send();
+    $response->sendFile($user_logo);
 }
 else {
     $origin_logo = new SparkFile(LOGO_NAME);
     $origin_logo->setPath(LOGO_PATH);
-    $response->setFile($origin_logo);
-    $response->send();
+    $response->sendFile($origin_logo);
 }
 ?>

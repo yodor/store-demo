266f51a6aadc809dbfdbc280d4dbbb361e78ce7f
Todor Gyumyushev <yodor1@gmail.com>
2024-09-07 21:24:02 +0300

    refactoring

--- admin1/clients/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: list.php");
-exit;
-?>
--- admin2/settings/admins/list.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once("session.php");
-include_once("templates/admin/AdminUsersListPage.php");
-$cmp = new AdminUsersListPage();
-$cmp->getPage()->navigation()->clear();
-$cmp->render();
-?>

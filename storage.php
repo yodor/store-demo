b5aad27cf2717bafda32e60509a9f5cf3d41153a
Todor Gyumyushev <yodor1@gmail.com>
2024-09-26 12:42:51 +0300

    refactoring/cleanups

--- storage.php
+++ storage.php
@@ -1,11 +1,5 @@
 <?php
 define("STORAGE_REQUEST", 1);
-
-include_once("session.php");
-Session::Close();
-
-$GLOBALS["DEBUG_OUTPUT"] = 0;
-
 include_once("storage/BeanDataRequest.php");
 $storage = new BeanDataRequest();
 ?>
